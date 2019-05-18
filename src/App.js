7144b7bae5f85eb4219a351bfbb716614a6c7da3
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-05-18 18:41:41 +0200

    module-2 conditionaly render elements

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
             {name: 'Zoki', age: 36},
             {name: 'Jeka', age: 30},
             {name: 'Aki', age: 2}
-        ]
+        ],
+        otherState: 'some other test will not be changed!',
+        showPersons: false
     };
 
     switchNameHandler = (newName) => {
@@ -39,6 +41,14 @@ class App extends Component {
         )
     };
 
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            //showPersons: this.state.showPersons ? false : true
+            showPersons: !doesShow // ovako je bolje, radi isto
+        })
+    };
+
     render() {
         const buttonStyle = {
           backgroundColor: 'lightBlue',
@@ -46,22 +56,36 @@ class App extends Component {
           padding: '8px',
           cursor: 'pointer'
         };
+
+        let persons = null;
+
+        if(this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        customClick={this.switchNameHandler.bind(this, 'Zokiiii!!!')}
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}
+                    >Moj hobi je trcanje</Person>
+                    <Person
+                        changeOnInput={this.nameChangeHandler}
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}/>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>React complete guide</h1>
-                <button style={buttonStyle} onClick={() => this.switchNameHandler('Zoran')}>Switch name</button>
-                <Person
-                    customClick={this.switchNameHandler.bind(this, 'Zokiiii!!!')}
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}
-                >Moj hobi je trcanje</Person>
-                <Person
-                    changeOnInput={this.nameChangeHandler}
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}/>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}/>
+                <button style={buttonStyle}
+                        onClick={this.togglePersonsHandler}>
+                        {this.state.showPersons ? 'hide' : 'show'} persons
+                </button>
+                {persons}
             </div>
         );
     }
