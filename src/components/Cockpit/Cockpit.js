9171159a6bb2e281e5b6319fbf022298ed41b4dc
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-05-20 22:34:33 +0200

    dynamically update inline style

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 const cockpit = ( props ) => {
     const buttonStyle = {
-              backgroundColor: 'lightBlue',
-              border:'1px solid blue',
-              padding: '8px',
-              cursor: 'pointer'
-            };
+        backgroundColor: 'green',
+        color: 'white',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    };
+
+    props.showPersons ? buttonStyle.backgroundColor = 'red' : buttonStyle.backgroundColor= 'green';
 
     return (
         <div>
             <h1>React complete guide</h1>
             <button style={buttonStyle}
                     onClick={props.toggle}>
-                    {props.showPersons ? 'hide' : 'show'} persons
+                    toggle persons
             </button>
         </div>
     )
