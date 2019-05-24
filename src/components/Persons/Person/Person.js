4a0214a35c0f8e8273e0e55cf526e09e2844986d
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-05-24 22:20:33 +0200

    Using wrapper Aux to return multiple top level elements next to each other

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux';
 
 //const person = ( props ) => {
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className="">
+            <Aux>
                 <p onClick={this.props.customClick}>
                     I am {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changeOnInput} value={this.props.name}/>
-            </div>
+            </Aux>
         )
     };
 }
---
+++ src/hoc/Aux.js
@@ -0,0 +1,8 @@
+//hoc stands for Higher order Components
+
+const aux = props => props.children;
+// this Higher order component is just used to return anything which is passed to it
+// means we can use it as a wrapper in JSX to allow multiple top level elements next to each other
+// it is important to return just one expressions (JS rule)
+
+export default aux;
